657b37a781c76afe5ac168915f2a4b1a30bcd7be
Matt <rmattsampson@gmail.com>
2023-02-11 10:27:10 -0500

    Update CustomTransform_FillEmptyStringsInAColumn.py
    handling both null and empty string cases

--- CustomTransform_FillEmptyStringsInAColumn.py
+++ CustomTransform_FillEmptyStringsInAColumn.py
@@ -3,14 +3,34 @@ from awsglue import DynamicFrame
 from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.types import StructType
 from pyspark.context import SparkContext
+import pandas as pd
 from pyspark.sql.functions import col,isnan, when, count, regexp_replace
 #from typing import Union
 
-def fill_null_values_txn(self, columnName, newValue):
-        gluectx = self.glue_ctx
+
+def fill_null_values_txn(
+    self, columnName, newValue
+):      
+        gluectx = self.glue_ctx       
         _df = self.toDF()
-        modifiedDF = _df.withColumn(columnName,when(col(columnName)=="" , newValue).otherwise(col(columnName)))
-        _dyf = DynamicFrame.fromDF(modifiedDF, self.glue_ctx, self.name)
-        return _dyf
+        if _df.filter(col(columnName)=="").count() > 0:
+            modifiedDF = _df.withColumn(columnName,when(col(columnName)=="" , newValue).otherwise(col(columnName)))
+            _dyf = DynamicFrame.fromDF(modifiedDF, self.glue_ctx, self.name)
+            return _dyf
+        elif _df.filter(col(columnName).isNull()).count() > 0:  
+            _pdf = _df.toPandas()
+            _pdf[columnName] = _pdf[columnName].fillna(newValue)
+            modifiedDF = gluectx.spark_session.createDataFrame(_pdf) 
+            _dyf = DynamicFrame.fromDF(modifiedDF, self.glue_ctx, self.name)
+            return _dyf
+        return self
+
+
+#def fill_null_values_txn(self, columnName, newValue):
+#        gluectx = self.glue_ctx
+#        _df = self.toDF()
+#        modifiedDF = _df.withColumn(columnName,when(col(columnName)=="" , newValue).otherwise(col(columnName)))
+#        _dyf = DynamicFrame.fromDF(modifiedDF, self.glue_ctx, self.name)
+#        return _dyf
 
 DynamicFrame.fill_null_values_txn = fill_null_values_txn
